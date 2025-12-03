bc9c474579d951755bcadc60a0fc87dc393b21dc
Darren Jones <Darren.Jones@odfl.com>
2025-12-03 09:30:02 -0500

    api k6 script add healthcheck requests

--- k6/api_test/main.js
+++ k6/api_test/main.js
@@ -20,6 +20,15 @@ export const options = {
     'http_req_blocked{request_type:sb_async}': ['p(95)<500'], // 95 percentile of the requests must not be blocked more than 0.5 seconds
     'http_req_waiting{request_type:sb_async}': ['p(95)<500'], // 95 percentile of the requests must not be waiting more than 0.5 seconds
     'http_req_duration{request_type:sb_async}': ['p(95)<10000'], // 95 percentile of requests must complete below 10s for sb_async
+
+    'http_req_failed{request_type:healthcheck_sb_basic}': ['rate<0.01'], // http errors should be less than 1% for healthcheck
+    'http_req_duration{request_type:healthcheck_sb_basic}': ['p(95)<200'], // 95 percentile of requests must complete below 200ms for healthcheck
+
+    'http_req_failed{request_type:healthcheck_sb_basic_vt}': ['rate<0.01'], // http errors should be less than 1% for healthcheck
+    'http_req_duration{request_type:healthcheck_sb_basic_vt}': ['p(95)<200'], // 95 percentile of requests must complete below 200ms for healthcheck
+
+    'http_req_failed{request_type:healthcheck_sb_async}': ['rate<0.01'], // http errors should be less than 1% for healthcheck
+    'http_req_duration{request_type:healthcheck_sb_async}': ['p(95)<200'], // 95 percentile of requests must complete below 200ms for healthcheck
   },
   scenarios: {
     scenario_process_value_sb_basic: {
@@ -55,6 +64,46 @@ export const options = {
       tags: { request_type: 'sb_async' },
       env: { HTTP_PROTO: 'http', BASE_URL: 'localhost', BASE_PORT: '8083',  URI: '/lookup' }
     },
+
+    healthcheck_sb_basic: {
+      executor: 'constant-arrival-rate',
+      exec: "testHealthLiveliness",
+      startTime: '1s',
+      duration: '30s',
+      rate: '2',
+      timeUnit: '1s',
+      gracefulStop: '10s',
+      preAllocatedVUs: '2',
+      maxVUs: '2', // maxx virtual users at a given time
+      tags: { request_type: 'healthcheck_sb_basic' },
+      env: { HTTP_PROTO: 'http', BASE_URL: 'localhost', BASE_PORT: '8081',  URI: '/actuator/liveness' }
+    },
+    healthcheck_sb_basic_vt: {
+      executor: 'constant-arrival-rate',
+      exec: "testHealthLiveliness",
+      startTime: '1s',
+      duration: '30s',
+      rate: '2',
+      timeUnit: '1s',
+      gracefulStop: '10s',
+      preAllocatedVUs: '2',
+      maxVUs: '2', // maxx virtual users at a given time
+      tags: { request_type: 'healthcheck_sb_basic_vt' },
+      env: { HTTP_PROTO: 'http', BASE_URL: 'localhost', BASE_PORT: '8082',  URI: '/actuator/liveness' }
+    },
+    healthcheck_sb_async: {
+      executor: 'constant-arrival-rate',
+      exec: "testHealthLiveliness",
+      startTime: '1s',
+      duration: '30s',
+      rate: '2',
+      timeUnit: '1s',
+      gracefulStop: '10s',
+      preAllocatedVUs: '2',
+      maxVUs: '2', // maxx virtual users at a given time
+      tags: { request_type: 'healthcheck_sb_async' },
+      env: { HTTP_PROTO: 'http', BASE_URL: 'localhost', BASE_PORT: '8083',  URI: '/actuator/liveness' }
+    },
   },
   summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)', 'count'],
 }
@@ -86,4 +135,13 @@ export function testLookup(data) {
     'lookup response status is 200 or 404': (r) => r.status === 200 || r.status === 404,
     'response body matches reconstructed value': (r) => r.body === randomValue,
   })
+}
+
+export function testHealthLiveliness() {
+  const OPTIONS = {
+    headers: {
+      'Accept': '*/*'
+    }
+  };
+  const response = http.get(`${__ENV.HTTP_PROTO}://${__ENV.BASE_URL}:${__ENV.BASE_PORT}${__ENV.URI}`, null, OPTIONS);
 }
